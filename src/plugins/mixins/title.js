c2eb387279184ffbe2b3f6eb8204eff6f40a241e
JounQin <admin@1stg.me>
2017-06-24 13:27:07 +0800

    improve title mixin

--- src/plugins/mixins/title.js
+++ src/plugins/mixins/title.js
@@ -1,28 +1,28 @@
 function getTitle(vm) {
-  const {title} = vm.$options
-  if (title) {
-    return typeof title === 'function'
-      ? title.call(vm)
-      : title
-  }
+  let {title} = vm.$options
+  title = title && typeof title === 'function' ? title.call(vm) : title
+  return title && `vue-ssr | ${title}`
 }
 
 const serverTitleMixin = {
   created() {
     const title = getTitle(this)
     if (title) {
-      this.$ssrContext.title = `vue-ssr | ${title}`
+      this.$ssrContext.title = title
     }
   }
 }
 
-const clientTitleMixin = {
-  mounted() {
-    const title = getTitle(this)
-    if (title) {
-      document.title = `vue-ssr | ${title}`
-    }
+function changeTitle() {
+  const title = getTitle(this)
+  if (title) {
+    document.title = title
   }
+}
+
+const clientTitleMixin = {
+  activated: changeTitle,
+  mounted: changeTitle
 }
 
 export default __SERVER__ ? serverTitleMixin : clientTitleMixin
